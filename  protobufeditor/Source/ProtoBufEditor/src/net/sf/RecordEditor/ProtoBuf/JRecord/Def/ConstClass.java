1b153e85c9a57e94b888677b7cdda8db35fc54cb
bm_tas <bm_tas@yahoo.com.au>
2012-05-17 05:43:47 +0000

    Version 0.85 changes

---  protobufeditor/Source/ProtoBufEditor/src/net/sf/RecordEditor/ProtoBuf/JRecord/Def/ConstClass.java
+++  protobufeditor/Source/ProtoBufEditor/src/net/sf/RecordEditor/ProtoBuf/JRecord/Def/ConstClass.java
@@ -5,7 +5,7 @@ import net.sf.RecordEditor.utils.common.Parameters;
 import net.sf.RecordEditor.utils.swing.Combo.ComboOption;
 
 public class ConstClass {
-	public final static byte[] EMPTY_BYTE_ARRAY = {}; 
+	public final static byte[] EMPTY_BYTE_ARRAY = {};
 
 	public static final int COPYBOOK_PROTO = 1;
 	public static final int COPYBOOK_COMPILED_PROTO = 2;
@@ -15,24 +15,24 @@ public class ConstClass {
 		new ComboOption(COPYBOOK_COMPILED_PROTO, Consts.COPYBOOK_COMPILED_PROTO),
 	};
 	private static final ComboOption[] STRUCTURE_OPTIONS;
-	
+
 	static {
 		ProtoIOProvider p = ProtoIOProvider.getInstance();
 		STRUCTURE_OPTIONS = new ComboOption[p.getNumberOfEntries()];
 		for (int i = 0; i < STRUCTURE_OPTIONS.length; i++) {
-			STRUCTURE_OPTIONS[i] = new ComboOption(ProtoIOProvider.getInstance().getKey(i), 
+			STRUCTURE_OPTIONS[i] = new ComboOption(ProtoIOProvider.getInstance().getKey(i),
 					ProtoIOProvider.getInstance().getName(i).substring(12));
 		}
 	};
 
 	public static final String VAR_PROTO_IMBED_DIR = "ProtoImbedDir";
-	
+
 	public static final String VAR_PROTOBUF_COMPILE = "ProtoC";
 	public static final String VAR_PROTOBUF_COMPILE_OPTS = "ProtoCopts.";
-	
+
 	public static final int NUMBER_OF_PROTOC_OPTIONS = 8;
-	
-	
+
+
 	/**
 	 * @return the cOPYBOOK_LOADER
 	 */
@@ -41,6 +41,24 @@ public class ConstClass {
 	}
 
 
+	public static int getLoaderIndex(int loaderId) {
+		for (int i = 0; i < COPYBOOK_LOADER.length; i++) {
+			if (COPYBOOK_LOADER[i].index == loaderId) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+
+	public static int getFileStructureIndex(int structure) {
+		for (int i = 0; i < STRUCTURE_OPTIONS.length; i++) {
+			if (STRUCTURE_OPTIONS[i].index == structure) {
+				return i;
+			}
+		}
+		return 0;
+	}
 
 	/**
 	 * @return the sTRUCTURE_OPTIONS
@@ -55,11 +73,11 @@ public class ConstClass {
 	 */
 	public static final String getProtoC() {
 		String s = Parameters.getString(VAR_PROTOBUF_COMPILE);
-		
+
 		if (s == null || s.equals("")) {
 			s = "protoc";
 		}
-		
+
 		return s;
 	}
 }
